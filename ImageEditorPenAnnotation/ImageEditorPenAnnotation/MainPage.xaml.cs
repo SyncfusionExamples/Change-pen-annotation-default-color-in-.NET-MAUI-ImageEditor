115b8b5b035578a8785b718596e5576e6634a747
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2024-04-17 09:25:15 +0530

    KB sample added

---
+++ ImageEditorPenAnnotation/ImageEditorPenAnnotation/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.Maui.ImageEditor;
+
+namespace ImageEditorPenAnnotation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAnnotationSelected(object sender, AnnotationSelectedEventArgs e)
+        {
+            if (e.AnnotationSettings is ImageEditorShapeSettings shapeSettings)
+            {
+                shapeSettings.Color = Colors.CadetBlue;
+            }
+        }
+    }
+}
